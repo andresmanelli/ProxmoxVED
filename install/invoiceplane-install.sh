892a8a7609e7d4316a17600e6010a771a14bdeea
Andrés MANELLI <am@toroid.io>
2026-01-23 13:48:59 +0100

    fix install

--- install/invoiceplane-install.sh
+++ install/invoiceplane-install.sh
@@ -73,8 +73,7 @@ server {
 EOF
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/invoiceplane /etc/nginx/sites-enabled/invoiceplane
-stop_service nginx
-start_service nginx
+svc_restart nginx
 
 cp /opt/${APPLICATION}/ipconfig.php.example /opt/${APPLICATION}/ipconfig.php
 sed -i "s|IP_URL=.*|IP_URL=http://$(get_current_ip())|" /opt/${APPLICATION}/ipconfig.php
